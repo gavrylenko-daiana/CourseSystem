dbaa20028a2a6155dd42c22e094fa989d4c198e2
Даяна Гавриленко <>
2023-07-25 16:33:53 +0200

    fixed some lines

--- CourseSystem/CourseSystem/Controllers/HomeController.cs
+++ CourseSystem/CourseSystem/Controllers/HomeController.cs
@@ -1,7 +1,8 @@
 ﻿using BLL.Interfaces;
 using Core.Enums;
 using Core.Models;
-﻿using System.Diagnostics;
+using System.Diagnostics;
+using CourseSystem.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace UI.Controllers
