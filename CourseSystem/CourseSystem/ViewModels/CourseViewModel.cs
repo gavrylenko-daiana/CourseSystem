6f204ef222b0324f6987de893f7691f1507540c1
darhan1168 <darinahanina04@gmail.com>
2023-07-26 11:37:10 +0200

    Update CourseViewModel.cs

--- CourseSystem/CourseSystem/ViewModels/CourseViewModel.cs
+++ CourseSystem/CourseSystem/ViewModels/CourseViewModel.cs
@@ -1,7 +1,12 @@
+using Core.Models;
+
 namespace UI.ViewModels;
 
 public class CourseViewModel
 {
     public int Id { get; set; }
     public string Name { get; set; }
+    public List<Group> Groups { get; set; }
+    public List<EducationMaterial> EducationMaterials { get; set; }
+    public List<UserCourses> UserCourses { get; set; }
 }
