454b3e62f554691dd95de54b12f41cb66072e34b
HerasymovaV <128616324+HerasymovaV@users.noreply.github.com>
2023-07-26 23:13:19 +0300

    Created activity table and service
    Keep in mind - there is a new migration.

---
+++ CourseSystem/BLL/Interfaces/IActivityService.cs
@@ -0,0 +1,20 @@
+﻿using Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Interfaces
+{
+    public interface IActivityService : IGenericService<UserActivity>
+    {
+        Task AddAssignmentSubmitedActivity(AppUser student, Assignment assignment);
+
+        Task AddJoinedCourseActivity(AppUser user, Course course);
+
+        Task AddAssignmentCreatedActivity(AppUser teacher, Assignment assignment);
+
+        Task AddAssignmentMarkedActivity(AppUser teacher, UserAssignments userAssignments);
+    }
+}
