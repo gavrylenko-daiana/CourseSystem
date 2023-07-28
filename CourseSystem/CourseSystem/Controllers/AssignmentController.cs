02345b07531e7f0dcc8ca096b4bd37ad05479ed1
Katrina Sidorenko <ekaterinasidorenko0914@gmail.com>
2023-07-28 16:08:38 +0300

    CreateAssignmentViewModelCreation

--- CourseSystem/CourseSystem/Controllers/AssignmentController.cs
+++ CourseSystem/CourseSystem/Controllers/AssignmentController.cs
@@ -1,5 +1,9 @@
 ﻿using BLL.Interfaces;
+using Core.Helpers;
+using Core.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using UI.ViewModels;
 
 namespace UI.Controllers
 {
@@ -10,8 +14,41 @@ namespace UI.Controllers
         {
             _assignmentService = assignmentService;   
         }
-        public IActionResult Index()
+        public async Task<IActionResult> Index(int id) //here is passing group id
         {
+            //all group assignments view (for student - InProgress + AwaitedApproval, for teachers - All statuses) 
+            //button for cretion of new one
+
+            return View();
+        }
+
+        [HttpGet]
+        [Route("Create/{groupId}")]
+        public async Task<IActionResult> Create(int groupId)
+        {
+            //check if this group exist 
+
+            var assignmentVM = new CreateAssignmentViewModel() { GroupId = groupId };
+
+            return View(assignmentVM);
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "Teacher")]
+        public async Task<IActionResult> CreateAssignment(CreateAssignmentViewModel assignmentVM)
+        {
+            if(assignmentVM == null)
+                return View("Error");
+
+            if(!ModelState.IsValid)
+            {
+                TempData.TempDataMessage("Error", "Failed to create assignment");
+                return View("CreateAssignment", assignmentVM);
+            }
+
+            var assignment = new Assignment();
+            LibraryForMapping.MapTo<CreateAssignmentViewModel, Assignment>(assignmentVM, assignment);
+
             return View();
         }
     }
---
+++ CourseSystem/CourseSystem/ViewModels/CreateAssignmentViewModel.cs
@@ -0,0 +1,33 @@
+﻿using Core.Enums;
+using Core.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace UI.ViewModels
+{
+    public class CreateAssignmentViewModel
+    {
+        [Required(ErrorMessage = "Assignment name is required")]
+        [Display(Name = "* Assignment name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Assignment description is required")]
+        [Display(Name = "* Assignment name")]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "Start date is required")]
+        [Display(Name = "* Start date")]
+        [DataType(DataType.Date)]
+        public DateTime StartDate { get; set; }
+
+        [Required(ErrorMessage = "End date is required")]
+        [Display(Name = "* End date")]
+        [DataType(DataType.Date)]
+        public DateTime EndDate { get; set; }
+
+        [Required(ErrorMessage = "Assignment status is required")]
+        [Display(Name = "* Assignment status")]
+        public AssignmentAccess AssignmentAccess { get; set; }
+        public int GroupId { get; set; }
+        public List<AssignmentFile> AssignmentFiles { get; set; }
+    }
+}
