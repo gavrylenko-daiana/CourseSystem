62834a69be4a91954ca3e7f981a1d736b4a45923
darhan1168 <darinahanina04@gmail.com>
2023-07-30 11:26:51 +0200

    Create methods for selecting teachers

--- CourseSystem/CourseSystem/Controllers/CourseController.cs
+++ CourseSystem/CourseSystem/Controllers/CourseController.cs
@@ -1,4 +1,5 @@
 using BLL.Interfaces;
+using Core.Enums;
 using Core.Helpers;
 using Core.Models;
 using Microsoft.AspNetCore.Identity;
@@ -187,5 +188,50 @@ public class CourseController : Controller
         TempData["CourseId"] = id;
 
         return View(courseViewModel);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> SelectTeachers()
+    {
+        var courseId = (int)(TempData["CourseId"] ?? throw new InvalidOperationException());
+        var course = await _courseService.GetById(courseId);
+        
+        if (course == null)
+        {
+            return NotFound();
+        }
+
+        var userCoursesForCourse = course.UserCourses.Select(uc => uc.AppUserId).ToList();
+
+        var teachers = _userManager.Users
+            .Where(u => u.Role == AppUserRoles.Teacher)
+            .Select(u => new TeacherViewModel
+            {
+                Id = u.Id,
+                FirstName = u.FirstName,
+                LastName = u.LastName,
+                IsInvited = userCoursesForCourse.Contains(u.Id)
+            })
+            .ToList();
+
+        return View(teachers);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> SendInvitation(string teacherId, int courseId)
+    {
+        var teacher = await _userManager.FindByIdAsync(teacherId);
+        var course = await _courseService.GetById(courseId);
+
+        if (teacher == null || course == null)
+        {
+            return NotFound();
+        }
+
+        //await _courseService.AddTeacherToCourse(course, teacher);
+        
+        TempData["CourseId"] = course.Id;
+        
+        return RedirectToAction("SelectTeachers");
     }
 }
