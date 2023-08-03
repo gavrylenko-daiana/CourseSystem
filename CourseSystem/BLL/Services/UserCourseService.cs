5bc92e71b3265c0cf4507e5b30d9154f9ad1b1f2
darhan1168 <darinahanina04@gmail.com>
2023-08-03 14:13:08 +0200

    Update UserCourseService.cs
    - now when user add to course, all group in this course will add with this user in userGroup

--- CourseSystem/BLL/Services/UserCourseService.cs
+++ CourseSystem/BLL/Services/UserCourseService.cs
@@ -6,16 +6,32 @@ namespace BLL.Services;
 
 public class UserCourseService : GenericService<UserCourses>, IUserCourseService
 {
-    public UserCourseService(UnitOfWork unitOfWork) : base(unitOfWork)
+    private readonly IUserGroupService _userGroupService;
+    public UserCourseService(UnitOfWork unitOfWork, IUserGroupService userGroupService) : base(unitOfWork)
     {
         _unitOfWork = unitOfWork;
         _repository = unitOfWork.UserCoursesRepository;
+        _userGroupService = userGroupService;
     }
 
     public async  Task AddTeacherToCourse(Course course, AppUser teacher)
     {
         try
         {
+            if (course.Groups.Any())
+            {
+                foreach (var group in course.Groups)
+                {
+                    var userGroup = new UserGroups()
+                    {
+                        AppUser = teacher,
+                        Group = group,
+                    };
+                    
+                    await _userGroupService.CreateUserGroups(userGroup);
+                }
+            }
+            
             var courseTeacher = new UserCourses()
             {
                 Course = course,
