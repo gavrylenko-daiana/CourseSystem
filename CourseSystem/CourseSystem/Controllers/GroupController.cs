1e4ebda85ab287a2635ba1841c769da5f29ff70d
darhan1168 <darinahanina04@gmail.com>
2023-08-09 11:15:45 +0200

    Revert "All admin add in group at course"
    This reverts commit 04991408be029393e6f1a83d1b0b0f5ccd2a4145.

--- CourseSystem/CourseSystem/Controllers/GroupController.cs
+++ CourseSystem/CourseSystem/Controllers/GroupController.cs
@@ -77,18 +77,17 @@ public class GroupController : Controller
     public async Task<IActionResult> Create(GroupViewModel groupViewModel)
     {
         var courseId = (int)(TempData["CourseId"] ?? throw new InvalidOperationException());
-        var course = await _courseService.GetById(courseId);
         var currentUser = await _userManager.GetUserAsync(User);
 
-        if (currentUser == null || course == null)
+        if (currentUser == null)
         {
-            TempData.TempDataMessage("Error", "User or course not found");
+            TempData.TempDataMessage("Error", "User not found");
             return View(groupViewModel);
         }
 
         var group = new Group();
         groupViewModel.MapTo(group);
-        group.Course = course;
+        group.CourseId = courseId;
 
         var createResult = await _groupService.CreateGroup(group, currentUser);
 
