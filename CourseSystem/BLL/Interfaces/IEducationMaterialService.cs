6696b601f6a05ded6ea3a6487cc5f4cce9032139
Даяна Гавриленко <>
2023-08-09 16:44:26 +0200

    added education material service

---
+++ CourseSystem/BLL/Interfaces/IEducationMaterialService.cs
@@ -0,0 +1,18 @@
+using CloudinaryDotNet.Actions;
+using Core.Models;
+using Microsoft.AspNetCore.Http;
+
+namespace BLL.Interfaces;
+
+public interface IEducationMaterialService
+{
+    Task<UploadResult> AddFileAsync(IFormFile file);
+
+    Task<DeletionResult> DeleteFileAsync(string publicId);
+
+    Task<List<EducationMaterial>> GetAllMaterialAsync();
+
+    Task AddToGroup(IFormFile material, int? groupId, string url);
+
+    Task<EducationMaterial> GetByIdMaterialAsync(int id);
+}
---
+++ CourseSystem/BLL/Services/EducationMaterialService.cs
@@ -0,0 +1,100 @@
+using BLL.Interfaces;
+using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+using Core.Configuration;
+using Core.Enums;
+using Core.Helpers;
+using Core.Models;
+using DAL.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
+
+namespace BLL.Services;
+
+public class EducationMaterialService : GenericService<EducationMaterial>, IEducationMaterialService
+{
+    private readonly Cloudinary _cloudinary;
+    private readonly ICourseService _courseService;
+    
+    public EducationMaterialService(UnitOfWork unitOfWork, IOptions<CloudinarySettings> config, ICourseService courseService) : base(unitOfWork)
+    {
+        var acc = new Account(
+            config.Value.CloudName,
+            config.Value.ApiKey,
+            config.Value.ApiSecret
+        );
+        _cloudinary = new Cloudinary(acc);
+        _courseService = courseService;
+    }
+
+    public async Task<UploadResult> AddFileAsync(IFormFile file)
+    {
+        try
+        {
+            var uploadResult = new RawUploadResult();
+
+            if (file.Length > 0)
+            {
+                using var stream = file.OpenReadStream();
+                var uploadParams = new RawUploadParams
+                {
+                    File = new FileDescription(file.FileName, stream)
+                };
+                uploadResult = await _cloudinary.UploadAsync(uploadParams);
+            }
+
+            return uploadResult;
+        }
+        catch (Exception e)
+        {
+            throw new Exception($"File could not be loaded. ErrorMessage: {e.Message}");
+        }
+    }
+
+    public async Task<DeletionResult> DeleteFileAsync(string url)
+    {
+        try
+        {
+            var deleteParams = new DeletionParams(url);
+
+            return await _cloudinary.DestroyAsync(deleteParams);
+        }
+        catch (Exception e)
+        {
+            throw new Exception($"File could not be deleted. ErrorMessage: {e.Message}");
+        }
+    }
+
+    public async Task AddToGroup(IFormFile material, int? groupId, string url)
+    {
+        // var group = await _groupService.GetById(groupId);
+        //
+        // if (group == null) throw new Exception("failed to get group by groupId");
+
+        var materialFile = new EducationMaterial()
+        {
+            Name = material.FileName,
+            Url = url,
+            FileExtension = Path.GetExtension(material.FileName),
+            MaterialAccess = MaterialAccess.Group,
+            // GroupId = group.Id
+        };
+        
+        await Add(materialFile);
+        // await group.EducationMaterials.Add(materialFile);
+    }
+
+    public async Task<List<EducationMaterial>> GetAllMaterialAsync()
+    {
+        var materials = await GetAll();
+
+        return materials;
+    }
+    
+    public async Task<EducationMaterial> GetByIdMaterialAsync(int id)
+    {
+        var material = await GetById(id);
+
+        return material;
+    }
+}
