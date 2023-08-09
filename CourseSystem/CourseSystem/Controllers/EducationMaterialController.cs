1c9446d4d4500f370ef4926829f75fe8d5c15d3c
Даяна Гавриленко <>
2023-08-09 19:58:43 +0200

    added create and detail in controller

---
+++ CourseSystem/CourseSystem/Controllers/EducationMaterialController.cs
@@ -0,0 +1,68 @@
+using BLL.Interfaces;
+using CloudinaryDotNet.Actions;
+using Core.Helpers;
+using Microsoft.AspNetCore.Mvc;
+using UI.ViewModels;
+
+namespace UI.Controllers;
+
+public class EducationMaterialController : Controller
+{
+    private readonly IEducationMaterialService _educationMaterialService;
+    private readonly IGroupService _groupService;
+
+    public EducationMaterialController(IEducationMaterialService educationMaterial, IGroupService groupService)
+    {
+        _educationMaterialService = educationMaterial;
+        _groupService = groupService;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Index()
+    {
+        var materials = await _educationMaterialService.GetAllMaterialAsync();
+
+        return View(materials);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> CreateInGroup(int groupId)
+    {
+        var group = await _groupService.GetById(groupId);
+        
+        var materialViewModel = new CreateEducationMaterialViewModel
+        {
+            CourseId = group.CourseId,
+            GroupId = groupId
+        };
+
+        return View(materialViewModel);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateInGroup(CreateEducationMaterialViewModel viewModel)
+    {
+        if (!ModelState.IsValid)
+        {
+            TempData.TempDataMessage("Error", "Incorrect data. Please try again!");
+
+            return View(viewModel);
+        }
+        
+        UploadResult uploadResult = await _educationMaterialService.AddFileAsync(viewModel.UploadFile);
+
+        await _educationMaterialService.AddToGroup(viewModel.UploadFile, viewModel.GroupId, uploadResult.Url.ToString());
+        
+        return RedirectToAction("Index", "Course");
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Detail(int id)
+    {
+        var material = await _educationMaterialService.GetByIdMaterialAsync(id);
+
+        TempData["UploadResult"] = material.Url;
+
+        return View(material);
+    }
+}
