31b98510926e5cf13c66866cf333e32ba5a3927f
darhan1168 <darinahanina04@gmail.com>
2023-08-10 08:42:22 +0200

    Update AccountController.cs

--- CourseSystem/CourseSystem/Controllers/AccountController.cs
+++ CourseSystem/CourseSystem/Controllers/AccountController.cs
@@ -164,6 +164,12 @@ public class AccountController : Controller
             TempData.TempDataMessage("Error", "This email is already in use");
             return View(registerViewModel);
         }
+        
+        if (!ValidationHelpers.IsValidEmail(registerViewModel.Email))
+        {
+            ViewData.ViewDataMessage("Error", "Entered incorrect email. Please try again.");
+            return View(registerViewModel);
+        }
 
         var newUser = new AppUser();
         registerViewModel.MapTo(newUser);
@@ -213,8 +219,13 @@ public class AccountController : Controller
         }
         else
         {
-            TempData.TempDataMessage("Error",
-                "Your password must have at least 6 characters. Must have at least 1 capital letter character, 1 digit and 1 symbol to choose from (!@#$%^&*()_+=\\[{]};:<>|./?,-)");
+            var errorMessages = string.Empty;
+            foreach (var error in newUserResponse.Errors)
+            {
+                errorMessages += $"{error.Description}{Environment.NewLine}";
+            }
+            
+            TempData.TempDataMessage("Error", errorMessages);
             return View(registerViewModel);
         }
     }
