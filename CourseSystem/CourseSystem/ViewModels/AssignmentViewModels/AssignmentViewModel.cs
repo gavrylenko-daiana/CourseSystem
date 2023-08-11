a77196d697b664929195055d61909856167d9a75
Katrina Sidorenko <ekaterinasidorenko0914@gmail.com>
2023-08-11 14:40:46 +0300

    AllowedNullDescriptionMigration

--- CourseSystem/CourseSystem/ViewModels/AssignmentViewModels/AssignmentViewModel.cs
+++ CourseSystem/CourseSystem/ViewModels/AssignmentViewModels/AssignmentViewModel.cs
@@ -8,7 +8,7 @@ namespace UI.ViewModels.AssignmentViewModels
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Description { get; set; }
+        public string? Description { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
         public int GroupId { get; set; }
