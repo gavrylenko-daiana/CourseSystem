59406855f819171d39d92a0bfcc6dedde249c9f1
Даяна Гавриленко <>
2023-08-12 00:01:03 +0200

    update education material controller

--- CourseSystem/CourseSystem/Controllers/EducationMaterialController.cs
+++ CourseSystem/CourseSystem/Controllers/EducationMaterialController.cs
@@ -66,9 +66,18 @@ public class EducationMaterialController : Controller
             TempData.TempDataMessage("Error", $"Message: {fullPath.Message}");
             return View(viewModel);
         }
+        
+        var group = await _groupService.GetById(viewModel.GroupId);
+
+        if (group == null)
+        {
+            TempData.TempDataMessage("Error", $"Failed to get {nameof(group)} by id {viewModel.GroupId}");
+            return RedirectToAction("Index", "Course");
+        }
 
         var addResult = await _educationMaterialService.AddToGroup(viewModel.UploadFile,
-            fullPath.Message, viewModel.GroupId);
+            fullPath.Message, group);
+        await _groupService.UpdateGroup(addResult.Data);
 
         if (!addResult.IsSuccessful)
         {
@@ -105,11 +114,19 @@ public class EducationMaterialController : Controller
             TempData.TempDataMessage("Error", $"Message: Failed to upload file");
             return View(viewModel);
         }
+        
+        var group = await _groupService.GetById(viewModel.SelectedGroupId);
+
+        if (group == null)
+        {
+            TempData.TempDataMessage("Error", $"Failed to get {nameof(group)} by id {viewModel.SelectedGroupId}");
+            return RedirectToAction("Index", "Course");
+        }
 
         if (viewModel.MaterialAccess == MaterialAccess.Group)
         {
             var addToGroupResult = await _educationMaterialService.AddToGroup(viewModel.UploadFile,
-                fullPath.Message, viewModel.SelectedGroupId);
+                fullPath.Message, group);
             
             if (!addToGroupResult.IsSuccessful)
             {
@@ -131,6 +148,58 @@ public class EducationMaterialController : Controller
 
         return RedirectToAction("Details", "Course", new { id = viewModel.CourseId });
     }
+    
+    // [HttpGet]
+    // public async Task<IActionResult> CreateInGeneral()
+    // {
+    //     var courses = await _courseService.GetAllCoursesAsync();
+    //     var groups = await _groupService.GetAllGroupsAsync();
+    //
+    //     var materialViewModel = new CreateInGeneralEducationMaterialViewModel
+    //     {
+    //         Courses = courses.Data!,
+    //         Groups = groups.Data!
+    //     };
+    //
+    //     return View(materialViewModel);
+    // }
+    //
+    // [HttpPost]
+    // public async Task<IActionResult> CreateInGeneral(CreateInGeneralEducationMaterialViewModel viewModel)
+    // {
+    //     var fullPath = await _educationMaterialService.AddFileAsync(viewModel.UploadFile);
+    //
+    //     if (!fullPath.IsSuccessful)
+    //     {
+    //         TempData.TempDataMessage("Error", $"Message: Failed to upload file");
+    //         return View(viewModel);
+    //     }
+    //
+    //     if (viewModel.MaterialAccess == MaterialAccess.Group)
+    //     {
+    //         var addToGroupResult = await _educationMaterialService.AddToGroup(viewModel.UploadFile,
+    //             fullPath.Message, viewModel.SelectedGroupId);
+    //         
+    //         if (!addToGroupResult.IsSuccessful)
+    //         {
+    //             TempData.TempDataMessage("Error", $"Message: {addToGroupResult.Message}");
+    //             return View(viewModel);
+    //         }
+    //     }
+    //     else
+    //     {
+    //         var addToCourseResult = await _educationMaterialService.AddToCourse(viewModel.UploadFile,
+    //             fullPath.Message, viewModel.CourseId);
+    //         
+    //         if (!addToCourseResult.IsSuccessful)
+    //         {
+    //             TempData.TempDataMessage("Error", $"Message: {addToCourseResult.Message}");
+    //             return View(viewModel);
+    //         }
+    //     }
+    //
+    //     return RedirectToAction("Details", "Course", new { id = viewModel.CourseId });
+    // }
 
     [HttpGet]
     public async Task<IActionResult> Detail(int id)
@@ -160,6 +229,7 @@ public class EducationMaterialController : Controller
         }
 
         var deleteResult = await _educationMaterialService.DeleteFileFromGroup(fileToDelete.Data);
+        await _groupService.UpdateGroup(deleteResult.Data);
 
         if (!deleteResult.IsSuccessful)
         {
