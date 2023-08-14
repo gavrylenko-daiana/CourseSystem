3f3b43c35c8640f76d0e394effcaeeeee20fbb93
darhan1168 <darinahanina04@gmail.com>
2023-08-14 09:55:43 +0200

    Add method for updating progress in UserGroups

--- CourseSystem/BLL/Services/UserGroupService.cs
+++ CourseSystem/BLL/Services/UserGroupService.cs
@@ -29,4 +29,24 @@ public class UserGroupService : GenericService<UserGroups>, IUserGroupService
             return new Result<bool>(false, $"Failed to create userGroups {userGroups.Id}. Exception: {ex.Message}");
         }
     }
+
+    public async Task<Result<bool>> UpdateProgressInUserGroups(UserGroups userGroups, double progress)
+    {
+        if (userGroups == null)
+        {
+            return new Result<bool>(false, $"{nameof(userGroups)} not found");
+        }
+        
+        try
+        {
+            await _repository.UpdateAsync(userGroups);
+            await _unitOfWork.Save();
+            
+            return new Result<bool>(true);
+        }
+        catch (Exception ex)
+        {
+            return new Result<bool>(false, $"Failed to update progress {progress} in userGroups with {userGroups.Id}. Exception: {ex.Message}");
+        }
+    }
 }
