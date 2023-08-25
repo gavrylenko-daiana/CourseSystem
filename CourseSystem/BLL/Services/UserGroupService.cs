836193559bf9c7d4e826145c9458d41e1ee73fe9
darhan1168 <darinahanina04@gmail.com>
2023-08-25 09:45:18 +0200

    Update UserGroupService.cs

--- CourseSystem/BLL/Services/UserGroupService.cs
+++ CourseSystem/BLL/Services/UserGroupService.cs
@@ -1,19 +1,27 @@
+using System.Reflection;
 using BLL.Interfaces;
 using Core.Models;
 using DAL.Repository;
+using Microsoft.Extensions.Logging;
 
 namespace BLL.Services;
 
 public class UserGroupService : GenericService<UserGroups>, IUserGroupService
 {
-    public UserGroupService(UnitOfWork unitOfWork) : base(unitOfWork, unitOfWork.UserGroupsRepository)
+    private readonly ILogger<UserGroupService> _logger;
+    
+    public UserGroupService(UnitOfWork unitOfWork, ILogger<UserGroupService> logger) 
+        : base(unitOfWork, unitOfWork.UserGroupsRepository)
     {
+        _logger = logger;
     }
 
     public async Task<Result<bool>> CreateUserGroups(UserGroups userGroups)
     {
         if (userGroups == null)
         {
+            _logger.LogError("Failed to {action}, {entity} was null!", MethodBase.GetCurrentMethod()?.Name, nameof(userGroups));
+            
             return new Result<bool>(false, $"{nameof(userGroups)} not found");
         }
 
@@ -22,10 +30,16 @@ public class UserGroupService : GenericService<UserGroups>, IUserGroupService
             await _repository.AddAsync(userGroups);
             await _unitOfWork.Save();
 
+            _logger.LogInformation("Successfully {action} in group {groupName} for user {userId}",
+                MethodBase.GetCurrentMethod()?.Name, userGroups.Group.Name, userGroups.AppUser.Id);
+            
             return new Result<bool>(true);
         }
         catch (Exception ex)
         {
+            _logger.LogError("Failed to {action} in group {groupName} for user {userId}. Error: {errorMsg}!", 
+                MethodBase.GetCurrentMethod()?.Name, userGroups.Group.Name, userGroups.AppUser.Id, ex.Message);
+            
             return new Result<bool>(false, $"Failed to create userGroups {userGroups.Id}. Exception: {ex.Message}");
         }
     }
@@ -34,6 +48,8 @@ public class UserGroupService : GenericService<UserGroups>, IUserGroupService
     {
         if (userGroups == null)
         {
+            _logger.LogError("Failed to {action}, {entity} was null!", MethodBase.GetCurrentMethod()?.Name, nameof(userGroups));
+
             return new Result<bool>(false, $"{nameof(userGroups)} not found");
         }
         
@@ -42,10 +58,16 @@ public class UserGroupService : GenericService<UserGroups>, IUserGroupService
             await _repository.UpdateAsync(userGroups);
             await _unitOfWork.Save();
             
+            _logger.LogInformation("Successfully {action} in group {groupName} for user {userId} with new progress {progress}",
+                MethodBase.GetCurrentMethod()?.Name, userGroups.Group.Name, userGroups.AppUser.Id, progress);
+            
             return new Result<bool>(true);
         }
         catch (Exception ex)
         {
+            _logger.LogError("Failed to {action} in group {groupName} for user {userId} with new progress {progress}. Error: {errorMsg}!", 
+                MethodBase.GetCurrentMethod()?.Name, userGroups.Group.Name, userGroups.AppUser.Id, progress, ex.Message);
+            
             return new Result<bool>(false, $"Failed to update progress {progress} in userGroups with {userGroups.Id}. Exception: {ex.Message}");
         }
     }
